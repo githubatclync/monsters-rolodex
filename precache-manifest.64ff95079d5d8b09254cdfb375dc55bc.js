5acd850163337ac1ea6b662921e7a73d13936565
Jason Braswell <pseudolus@gmail.com>
2019-12-28 03:03:21 -0500

    Updates

---
+++ precache-manifest.64ff95079d5d8b09254cdfb375dc55bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4cbc1355db12b316a057d32ef1b601",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f06a2b7e41ed275d22b3",
+    "url": "/monsters-rolodex/static/css/main.2f3ec487.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f06a2b7e41ed275d22b3",
+    "url": "/monsters-rolodex/static/js/main.e5a89bef.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
